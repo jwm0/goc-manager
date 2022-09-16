71cf450593fca99c131bb9852cd89c2d25df15d0
Jacek Kucharczyk <jacek.kucharczyk@databricks.com>
2022-09-16 19:37:43 +0200

    Allow editing GOC values

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -12,17 +12,24 @@ import {
   Button,
   ButtonGroup,
   Input,
+  Heading,
+  Divider,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
+import { sendToContentScript } from '../messages';
 import './styles.scss';
-import { Flag, Value } from '../types';
+import { Config, Flag, Value } from '../types';
 
 function openWebPage(url: string): Promise<Tabs.Tab> {
   return browser.tabs.create({ url });
 }
 
 const getDisplayValue = (value: Value): string => {
+  if (typeof value === 'undefined') {
+    return '';
+  }
+
   if (typeof value === 'string') {
     return `"${value}"`;
   }
@@ -34,9 +41,113 @@ const getDisplayValue = (value: Value): string => {
   return String(value);
 };
 
-function FeatureList({ flags }: { flags: Flag[] }) {
+const getParsedValue = (value: string, fallbackValue: unknown): unknown => {
+  if (value.startsWith('"') && value.endsWith('"')) {
+    return value.slice(1, -1);
+  }
+
+  try {
+    return JSON.parse(value);
+  } catch (e) {
+    return fallbackValue;
+  }
+};
+
+function FlagRow({
+  name,
+  overridden,
+  value,
+  overriddenValue,
+  unsavedValue,
+  setOverride,
+}: {
+  name: string;
+  overridden: boolean;
+  value: string;
+  overriddenValue: string;
+  unsavedValue?: string;
+  setOverride: (key: string, value: Value) => void;
+}) {
+  const [editing, setEditing] = useState(false);
+  const [localOverride, setLocalOverride] = useState<string>();
+  const currentValue = overridden ? overriddenValue : value;
+
+  const displayValue = () => {
+    if (unsavedValue) {
+      return <b>{unsavedValue}*</b>;
+    }
+
+    if (overridden) {
+      return <b>{currentValue}</b>;
+    }
+
+    return currentValue;
+  };
+
+  const handleEdit = () => {
+    if (editing) {
+      if (localOverride) {
+        const parsedValue = getParsedValue(localOverride, currentValue);
+        setOverride(name, parsedValue);
+      }
+      setEditing(false);
+    } else {
+      setEditing(true);
+    }
+  };
+
   return (
-    <TableContainer>
+    <Tr>
+      <Td>{name}</Td>
+      <Td
+        css={{
+          minWidth: 200,
+          maxWidth: 200,
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+        }}
+      >
+        {editing ? (
+          <Input
+            placeholder={currentValue}
+            value={localOverride}
+            onChange={(e) => setLocalOverride(e.target.value)}
+          />
+        ) : (
+          displayValue()
+        )}
+      </Td>
+      <Td>
+        <ButtonGroup variant="outline" spacing="6">
+          <Button size="xs" colorScheme="teal" onClick={handleEdit}>
+            Edit
+          </Button>
+          {overridden && (
+            <Button
+              size="xs"
+              colorScheme="teal"
+              onClick={() => setOverride(name, undefined)}
+            >
+              Reset to default
+            </Button>
+          )}
+        </ButtonGroup>
+      </Td>
+    </Tr>
+  );
+}
+
+function FeatureList({
+  flags,
+  localOverrides,
+  setOverride,
+}: {
+  flags: Flag[];
+  localOverrides: Config | undefined;
+  setOverride: (key: string, value: Value) => void;
+}) {
+  return (
+    <TableContainer minHeight={250} maxHeight={250} overflowY="scroll">
       <Table variant="striped" size="sm">
         <Thead>
           <Tr>
@@ -50,22 +161,22 @@ function FeatureList({ flags }: { flags: Flag[] }) {
             const overridden = typeof flag.overriddenValue !== 'undefined';
             const value = getDisplayValue(flag.value);
             const overriddenValue = getDisplayValue(flag.overriddenValue);
+            const unsavedValue = getDisplayValue(localOverrides?.[flag.name]);
 
             return (
-              <Tr key={flag.name}>
-                <Td>{flag.name}</Td>
-                <Td>{overridden ? <b>{overriddenValue}*</b> : value}</Td>
-                <Td>
-                  <ButtonGroup variant="outline" spacing="6">
-                    <Button size="xs" colorScheme="teal">
-                      Override
-                    </Button>
-                    <Button size="xs" colorScheme="teal">
-                      Reset to default
-                    </Button>
-                  </ButtonGroup>
-                </Td>
-              </Tr>
+              <FlagRow
+                key={flag.name}
+                name={flag.name}
+                overridden={overridden}
+                value={value}
+                overriddenValue={overriddenValue}
+                setOverride={setOverride}
+                unsavedValue={
+                  Object.hasOwn(localOverrides || {}, flag.name)
+                    ? unsavedValue
+                    : undefined
+                }
+              />
             );
           })}
         </Tbody>
@@ -77,38 +188,85 @@ function FeatureList({ flags }: { flags: Flag[] }) {
 function Root() {
   const [search, setSearch] = useState('');
   const [flags, setFlags] = useState<Flag[]>([]);
+  const [unsavedOverrides, setUnsavedOverrides] = useState<Config>();
 
   const filteredFlags = flags.filter(({ name }) =>
     name.toLowerCase().includes(search.toLocaleLowerCase())
   );
 
   const setOverride = (key: string, value: unknown) => {
-    // browser.runtime.sendMessage("")
+    setUnsavedOverrides((overrides) => ({
+      ...overrides,
+      [key]: value,
+    }));
+  };
+
+  const submit = () => {
+    if (!unsavedOverrides) {
+      return;
+    }
+    const overridesToRemove: Record<string, true> = {};
+
+    Object.keys(unsavedOverrides).forEach((key) => {
+      if (unsavedOverrides[key] === undefined) {
+        overridesToRemove[key] = true;
+      }
+    });
+
+    sendToContentScript({
+      origin: 'extension',
+      type: 'set-overrides',
+      data: { overridesToRemove, overridesToUpdate: unsavedOverrides },
+    });
+  };
+
+  const init = () => {
+    sendToContentScript({
+      origin: 'extension',
+      type: 'init',
+    });
   };
 
   useEffect(() => {
-    browser.runtime.onMessage.addListener(
-      (message: { type: string; data: Flag[] }) => {
-        console.log('popup', message);
-        if (message.type === 'goc-manager-agent') {
-          setFlags(message.data);
-        }
+    init();
+
+    const handler = (message: { type: string; data: Flag[] }) => {
+      if (message.type === 'goc-manager-agent') {
+        setFlags(message.data);
       }
-    );
+    };
+
+    browser.runtime.onMessage.addListener(handler);
+
+    return () => {
+      browser.runtime.onMessage.removeListener(handler);
+    };
   }, []);
 
   return (
     <ChakraProvider>
       <section id="popup">
-        <h2>GOC manager</h2>
-        <Input value={search} onChange={(e) => setSearch(e.target.value)} />
-        <FeatureList flags={filteredFlags} />
+        <Heading size="lg" colorScheme="teal" marginBottom={5}>
+          Config manager
+        </Heading>
+        <Input
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          css={{ marginBottom: 10 }}
+        />
+        <FeatureList
+          flags={filteredFlags}
+          setOverride={setOverride}
+          localOverrides={unsavedOverrides}
+        />
+        <Divider marginBottom={5} />
         <Button
-          onClick={(): Promise<Tabs.Tab> => {
-            return openWebPage('options.html');
-          }}
+          onClick={submit}
+          colorScheme="teal"
+          disabled={!unsavedOverrides}
         >
-          Options Page
+          Apply and reload
         </Button>
       </section>
     </ChakraProvider>
